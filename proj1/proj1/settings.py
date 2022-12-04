53d7ffaa334c57db883ef53f6fa3905337454312
Osama2822 <119732431+Osama2822@users.noreply.github.com>
2022-12-04 16:04:27 +0500

    Update settings.py

--- proj1/proj1/settings.py
+++ proj1/proj1/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'proj1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templat    es"],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
